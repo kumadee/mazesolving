116f6d307e809b8e8d404fa978e01286a375aeac
kumadee <deepak.kumar.ext@gmail.com>
2021-09-23 14:33:08 +0000

    chore: add simple test

---
+++ test_solve.py
@@ -0,0 +1,9 @@
+import unittest
+import tempfile
+
+from solve import solve
+
+class TestMaze(unittest.TestCase):
+    def test_default(self):
+        sf = SolverFactory()
+        solve(sf, sf.Default, "examples/normal.png", tempfile.mkstemp())
